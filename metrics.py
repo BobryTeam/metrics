5d207162f39f6fc55e5546de854797421e36ae5f
vitosotdihaet <vitostikitos@gmail.com>
2024-05-20 17:13:38 +0300

    Add a skeleton for Metrics class

---
+++ metrics.py
@@ -0,0 +1,19 @@
+class Metrics:
+    def __init__(self):
+        '''
+        Инициализация класса
+        '''
+        pass
+
+    def __str__(self) -> str:
+        '''
+        Превращает скейл дату в строку
+        '''
+        return ''
+
+class MetricsFromStr(Metrics):
+    def __init__(self, string_data: str):
+        '''
+        Инициализация класса из строки
+        '''
+        return super().__init__()
