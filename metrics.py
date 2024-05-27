bce8c06c7a86c50dec1a86636a7953894a3f019c
Vitaly Klimenko <67521698+vitosotdihaet@users.noreply.github.com>
2024-05-27 21:34:32 +0300

    Add a lambda function that computes the PromQL requests

--- metrics.py
+++ metrics.py
@@ -2,6 +2,19 @@ import json
 
 
 class Metrics:
+    PROMQL_REQUESTS = lambda target_job: [
+        f'sum(rate(node_cpu_seconds_total{{mode!="idle", job="{target_job}"}}[1m]))/ \
+        count(rate(node_cpu_seconds_total{{mode!="idle", job="{target_job}"}}[1m]) > bool 0.05)',
+
+        f'1 - ((avg(avg_over_time(node_memory_MemFree_bytes{{job="{target_job}"}}[1m])) + \
+        avg(avg_over_time(node_memory_Cached_bytes{{job="{target_job}"}}[1m])) + \
+        avg(avg_over_time(node_memory_Buffers_bytes{{job="{target_job}"}}[1m]))) / \
+        avg(avg_over_time(node_memory_MemTotal_bytes{{job="{target_job}"}}[1m])))',
+
+        f'avg(rate(node_network_receive_bytes_total{{job="{target_job}"}}[1m])) * 8 / 1024 / 1024',
+        f'avg(rate(node_network_transmit_bytes_total{{job="{target_job}"}}[1m])) * 8 / 1024 / 1024', 
+    ]
+
     def __init__(self, cpu_load: float, ram_load: float, net_in_load: float, net_out_load: float):
         '''
         Инициализация класса
