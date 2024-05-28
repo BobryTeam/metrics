08acfaa21e6cbbfa0bb3f523197c1e07b424d2b4
vitosotdihaet <vitostikitos@gmail.com>
2024-05-28 22:45:43 +0300

    Remove useless shit from setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ setup(
     author='BobryTeam',
     author_email='sinntexxx@gmail.com',
     description='Metrics data structure',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     url='https://github.com/BobryTeam/metrics',
     classifiers=[
         'Programming Language :: Python :: 3',
